b4d18ea63024ee383bb77b483f02dae2d977aff5
林家小院 <guolinit@163.com>
2018-12-21 18:35:35 +0800

    'modify'

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,7 @@
+import com.ctl.sys.manger.shiro.ShiroAuthorizationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * <p>Title: Test</p>
  * <p>Description: </p>
@@ -9,9 +13,12 @@
  * @date 2018-12-20 14:33
  */
 public class Test {
+    private static Logger log = LoggerFactory.getLogger(Test.class);
+
     public static void main(String[] args) {
         String str="/organization/treeGrid";
        int index=  str.lastIndexOf("/");
         System.out.println(str.substring(0,index)+":"+str.substring(index+1));
+        log.info("123");
     }
 }
